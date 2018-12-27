2ffa0f38f2421d9be6b6892f8337bb0d38637d1f
sunilkunchoor <sunilkunchoor@gmail.com>
2018-12-28 01:30:53 +0530

    Fixed Project Buttons and added CV link to about block

--- js/popup.js
+++ js/popup.js
@@ -2,43 +2,38 @@
 var popup = document.getElementById('myPopup');
 
 // Get the button that opens the modal
-var btn = document.getElementsByClassName("myBtn");
+var btn1 = document.getElementsByClassName("myBtn")[0];
+var btn2 = document.getElementsByClassName("myBtn")[1];
+
+// Get the button that opens the modal
+var pDetail = document.getElementsByClassName("projectDetail");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-// Heading for all explanations
-var head = [
-  "A",
-  "B",
-  "C",
-  "D"];
-
-// Explanation for each Projects
-var explanation = [
-  "a",
-  "b",
-  "c",
-  "d"];
-
 // When the user clicks the button, open the popup
-var i;
-for (i = 0; i < btn.length; i++) {
-  document.getElementById('text_head').innerHTML=head[i];
-  document.getElementById('text_expla').innerHTML=explanation[i];
-  btn[i].onclick = function() {
-    popup.style.display = "block";
-  }
+btn1.onclick = function() {
+  pDetail[0].style.display = "block";
+  popup.style.display = "block";
+}
+
+btn2.onclick = function() {
+  pDetail[1].style.display = "block";
+  popup.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the popup
 span.onclick = function() {
+  pDetail[0].style.display = "none";
+  pDetail[1].style.display = "none";
   popup.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the popup, close it
 window.onclick = function(event) {
   if (event.target == popup) {
+    pDetail[0].style.display = "none";
+    pDetail[1].style.display = "none";
     popup.style.display = "none";
   }
 }
