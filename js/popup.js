c07fa953d64e817b47afeaa14b28db7fb593a33a
Sunil Kunchoor <sunilkunchoor@gmail.com>
2021-02-03 08:20:19 +0530

    Update Portfolio

--- js/popup.js
+++ js/popup.js
@@ -4,6 +4,7 @@ var popup = document.getElementById('myPopup');
 // Get the button that opens the modal
 var btn0 = document.getElementsByClassName("myBtn")[0];
 var btn1 = document.getElementsByClassName("myBtn")[1];
+var btn2 = document.getElementsByClassName("myBtn")[2];
 
 // Get the button that opens the modal
 var pDetail = document.getElementsByClassName("projectDetail");
@@ -22,11 +23,16 @@ btn1.onclick = function() {
   popup.style.display = "block";
 }
 
+btn2.onclick = function() {
+  pDetail[2].style.display = "block";
+  popup.style.display = "block";
+}
 
 // When the user clicks on <span> (x), close the popup
 span.onclick = function() {
   pDetail[0].style.display = "none";
   pDetail[1].style.display = "none";
+  pDetail[2].style.display = "none";
   popup.style.display = "none";
 }
 
@@ -35,6 +41,7 @@ window.onclick = function(event) {
   if (event.target == popup) {
     pDetail[0].style.display = "none";
     pDetail[1].style.display = "none";
+	pDetail[2].style.display = "none";
     popup.style.display = "none";
   }
 }
